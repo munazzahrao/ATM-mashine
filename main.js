1a9b3dde562312c46952e78e33b539034a73d882
munazzahrao <161147860+munazzahrao@users.noreply.github.com>
2024-06-11 19:06:57 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+let myBalance = 15000;
+let myPin = 8542;
+let pinAnswer = await inquirer.prompt([{
+        name: "pin",
+        message: "enter your pin code",
+        type: "number",
+    }]);
+if (pinAnswer.pin === myPin) {
+    console.log("correct pin code!!!");
+    let operationAns = await inquirer.prompt([{
+            name: "operation",
+            message: "plz select option",
+            type: "list",
+            choices: ["withdraw", "check balance"]
+        }]);
+    console.log(operationAns);
+    if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer.prompt([{
+                name: "amount",
+                message: "enter your amount",
+                type: "number",
+            }]);
+        (myBalance -= amountAns.amount);
+        console.log("your remaining balance is: " + myBalance);
+    }
+    else if (operationAns.operation === "check balance") {
+        console.log("your balance is: " + myBalance);
+    }
+}
+else {
+    console.log("incorrect pin code!!!");
+}
+;
